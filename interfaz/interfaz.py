f01be207017af630b3375570c1cbf072f0344d06
cambiummgh <miguel.ghidalgo@protonmail.com>
2021-10-29 12:27:00 +0200

    Commented Code Removed

--- interfaz/interfaz.py
+++ interfaz/interfaz.py
@@ -8,270 +8,172 @@ class Ui_interfaz(object):
     def setupUi(self, interfaz, data):
         interfaz.setObjectName("CaptuRinG")
         interfaz.resize(480, 500)
-        
+
         interfaz.setWindowIcon(QtGui.QIcon('icon.png'))
-        
+
         self.imageL = QtWidgets.QLabel(interfaz)
-        self.imageL.setGeometry(QtCore.QRect(50,380,200,80))
+        self.imageL.setGeometry(QtCore.QRect(50, 380, 200, 80))
         # loading image
         self.pixmap = QPixmap('CambiumLarge.jpg')
         self.scaledImg = self.pixmap.scaledToWidth(200)
         self.imageL.setPixmap(self.scaledImg)
-        
-        
+
         self.addressL = QtWidgets.QLabel(interfaz)
         self.addressL.setGeometry(QtCore.QRect(80, 460, 300, 40))
-        self.urlLink="<a href=\"http://www.cambiumresearch.eu\">'Cambium RG'</a>"
+        self.urlLink = "<a href=\"http://www.cambiumresearch.eu\">'Cambium RG'</a>"
         self.addressL.setOpenExternalLinks(True)
         self.addressL.setObjectName("addressL")
- 
-        self.myFont = QtGui.QFont() #Font type for titles
-        self.myFont.setBold(True) 
- 
+
+        self.myFont = QtGui.QFont()  # Font type for titles
+        self.myFont.setBold(True)
+
         self.shootingL = QtWidgets.QLabel(interfaz)
         self.shootingL.setGeometry(QtCore.QRect(10, 10, 300, 40))
         self.shootingL.setObjectName("shootingL")
         self.shootingL.setFont(self.myFont)
-        
+
         self.optionsL = QtWidgets.QLabel(interfaz)
         self.optionsL.setGeometry(QtCore.QRect(320, 10, 300, 40))
         self.optionsL.setObjectName("optionsL")
         self.optionsL.setFont(self.myFont)
-      
-        #Sliders
-        #self.horizontalSlider = QtWidgets.QSlider(interfaz)
-        #self.horizontalSlider.setGeometry(QtCore.QRect(160, 680, 800, 22))
-        #self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
-        #self.horizontalSlider.setSliderPosition(50)
-        #self.horizontalSlider.setTracking(True)
-        #self.horizontalSlider.setObjectName("horizontalSlider")
-        
-        #self.verticalSlider = QtWidgets.QSlider(interfaz)
-        #self.verticalSlider.setGeometry(QtCore.QRect(970, 55, 22, 600))
-        #self.verticalSlider.setSliderPosition(50)
-        #self.verticalSlider.setTracking(True)
-        #self.verticalSlider.setOrientation(QtCore.Qt.Vertical)
-        #self.verticalSlider.setObjectName("verticalSlider")
-        
-        #Panorama Name
-        #self.nameLabel = QtWidgets.QLabel(interfaz)
-        #self.nameLabel.setGeometry(QtCore.QRect(10, 10, 220, 16))
-        #self.nameLabel.setObjectName("nameLabel")
-        
-        #self.nombreMuestra = QtWidgets.QLineEdit(interfaz)
-        #self.nombreMuestra.setGeometry(QtCore.QRect(10, 30, 300, 21))
-        #self.nombreMuestra.setObjectName("nombreMuestra")
-        
-        
+
         #       Frame creado por MGH
         self.frame = QtWidgets.QFrame(interfaz)
         self.frame.setGeometry(QtCore.QRect(10, 50, 300, 300))
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
-        
+
         self.frame.setObjectName("frame")
-        
-        
-        ##Core Name
+
+        # Core Name
         self.nameCoreL = QtWidgets.QLabel(self.frame)
         self.nameCoreL.setGeometry(QtCore.QRect(30, 20, 165, 40))
         self.nameCoreL.setObjectName("nameCoreL")
-        
+
         self.nameCore = QtWidgets.QLineEdit(self.frame)
         self.nameCore.setGeometry(QtCore.QRect(30, 55, 130, 35))
         self.nameCore.setObjectName("nameCore")
-        
-        #New nameCore button
+
+        # New nameCore button
         self.newcoreName = QtWidgets.QPushButton(self.frame)
         self.newcoreName.setGeometry(QtCore.QRect(180, 55, 90, 35))
         self.newcoreName.setObjectName("newcoreName")
-        
-        #sampleSize
+
+        # sampleSize
         self.sampleSizeLabel = QtWidgets.QLabel(self.frame)
-        self.sampleSizeLabel.setGeometry(QtCore.QRect(30, 100, 165, 30)) 
+        self.sampleSizeLabel.setGeometry(QtCore.QRect(30, 100, 165, 30))
         self.sampleSizeLabel.setObjectName("sampleSizeLabel")
-             
+
         self.tamanioMuestra = QtWidgets.QSpinBox(self.frame)
-        self.tamanioMuestra.setGeometry(QtCore.QRect(30, 135, 130, 30)) 
+        self.tamanioMuestra.setGeometry(QtCore.QRect(30, 135, 130, 30))
         self.tamanioMuestra.setMaximum(99999)
         self.tamanioMuestra.setObjectName("tamanioMuestra")
         self.tamanioMuestra.setValue(data[4])
-        
-        #Crear Muestra
-        
+
+        # Crear Muestra
+
         self.crearMuestra = QtWidgets.QPushButton(self.frame)
         self.crearMuestra.setGeometry(QtCore.QRect(60, 190, 180, 60))
         self.crearMuestra.setStyleSheet
         self.crearMuestra.setObjectName("crearMuestra")
-        
-        #New Panorama
-        #self.newPanorama = QtWidgets.QPushButton(interfaz)
-        #self.newPanorama.setGeometry(QtCore.QRect(340, 30, 76, 22))
-        #self.newPanorama.setObjectName("newPanorama")
-               
-        #previewMuestra
-        #self.previewMuestra = QtWidgets.QGraphicsView(interfaz)
-        #self.previewMuestra.setGeometry(QtCore.QRect(160, 60, 500, 500))
-        #self.previewMuestra.setObjectName("previewMuestra")
-        
-#       Frame
-#         self.frame = QtWidgets.QFrame(interfaz)
-#         self.frame.setGeometry(QtCore.QRect(10, 60, 140, 230))
-#         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
-#         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
-#         self.frame.setObjectName("frame")
-        
-        #PanoramaList
-        #self.panoramasLabel = QtWidgets.QLabel(self.frame)
-        #self.panoramasLabel.setGeometry(QtCore.QRect(10, 10, 110, 22))
-        #self.panoramasLabel.setObjectName("panoramasLabel")
-        
-        #self.panoramasList = QtWidgets.QComboBox(self.frame)
-        #self.panoramasList.setGeometry(QtCore.QRect(10, 35, 110, 22))
-        #self.panoramasList.setMaxVisibleItems(99)
-        #self.panoramasList.setObjectName("panoramasList")
-        #self.panoramasList.addItem("")
-        
-        #Images
-        #self.imagenesLabel = QtWidgets.QLabel(self.frame)
-        #self.imagenesLabel.setGeometry(QtCore.QRect(20, 60, 91, 22))
-        #self.imagenesLabel.setObjectName("imagenesLabel")
-        
-        #self.imagenesList = QtWidgets.QComboBox(self.frame)
-        #self.imagenesList.setGeometry(QtCore.QRect(30, 82, 91, 22))
-        #self.imagenesList.setMaxVisibleItems(99)
-        #self.imagenesList.setObjectName("imagenesList")
-        #self.imagenesList.addItem("")
-        
-        #Obtain
-        #self.obtainImg = QtWidgets.QPushButton(self.frame)
-        #self.obtainImg.setGeometry(QtCore.QRect(30, 110, 91, 25)) 
-        #self.obtainImg.setObjectName("obtainImg")
-        #Add
-        #self.addImg = QtWidgets.QPushButton(self.frame)
-        #self.addImg.setGeometry(QtCore.QRect(30, 135, 91, 25)) 
-        #self.addImg.setObjectName("addImg")
-        #Remove
-        #self.removeImg = QtWidgets.QPushButton(self.frame)
-        #self.removeImg.setGeometry(QtCore.QRect(30, 160, 91, 25))
-        #self.removeImg.setObjectName("removeImg")
-                
-        #STITCH
-        #self.stitch = QtWidgets.QPushButton(self.frame)
-        #self.stitch.setGeometry(QtCore.QRect(30, 195, 91, 21)) 
-        #self.stitch.setObjectName("stitch")
-        
-        
-        
+
         # FRAME02
-        
+
         self.frame_2 = QtWidgets.QFrame(interfaz)
         self.frame_2.setGeometry(QtCore.QRect(320, 50, 140, 430))
         self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_2.setObjectName("frame_2")
-  
-        
-        #offset
+
+        # offset
         self.offsetLabel = QtWidgets.QLabel(self.frame_2)
-        self.offsetLabel.setGeometry(QtCore.QRect(10, 5, 120, 30)) 
+        self.offsetLabel.setGeometry(QtCore.QRect(10, 5, 120, 30))
         self.offsetLabel.setObjectName("offsetLabel")
-        
+
         self.offset = QtWidgets.QSpinBox(self.frame_2)
         self.offset.setGeometry(QtCore.QRect(10, 40, 120, 30))
         self.offset.setMaximum(99999)
         self.offset.setObjectName("offset")
         self.offset.setValue(data[1])
-        
-        #speed/step
+
+        # speed/step
         self.spstepLabel = QtWidgets.QLabel(self.frame_2)
         self.spstepLabel.setGeometry(QtCore.QRect(10, 75, 120, 30))
         self.spstepLabel.setObjectName("spstepLabel")
-        
+
         self.stepSpeed = QtWidgets.QSpinBox(self.frame_2)
-        self.stepSpeed.setGeometry(QtCore.QRect(10, 100, 120, 30)) 
+        self.stepSpeed.setGeometry(QtCore.QRect(10, 100, 120, 30))
         self.stepSpeed.setMaximum(99999)
         self.stepSpeed.setObjectName("stepSpeed")
         self.stepSpeed.setValue(data[2])
-                
-        #Initial speed
+
+        # Initial speed
         self.inSpeedLabel = QtWidgets.QLabel(self.frame_2)
         self.inSpeedLabel.setGeometry(QtCore.QRect(10, 135, 120, 30))
         self.inSpeedLabel.setObjectName("inSpeedLabel")
-        
+
         self.initSpeed = QtWidgets.QSpinBox(self.frame_2)
         self.initSpeed.setGeometry(QtCore.QRect(10, 170, 120, 30))
         self.initSpeed.setMaximum(99999)
         self.initSpeed.setObjectName("initSpeed")
         self.initSpeed.setValue(data[3])
-                       
+
         ######
         #####
-        
-        #sizeStep
+
+        # sizeStep
         self.sizeStepLabel = QtWidgets.QLabel(self.frame_2)
         self.sizeStepLabel.setGeometry(QtCore.QRect(10, 205, 120, 30))
         self.sizeStepLabel.setObjectName("sizeStepLabel")
-        
+
         self.sizeStep = QtWidgets.QSpinBox(self.frame_2)
         self.sizeStep.setGeometry(QtCore.QRect(10, 240, 120, 30))
         self.sizeStep.setObjectName("sizeStep")
         self.sizeStep.setMaximum(99999)
         self.sizeStep.setValue(data[5])
-        
-        
-        #spindleSize
+
+        # spindleSize
         self.spindleSizeLabel = QtWidgets.QLabel(self.frame_2)
-        self.spindleSizeLabel.setGeometry(QtCore.QRect(10, 275, 120, 30)) 
+        self.spindleSizeLabel.setGeometry(QtCore.QRect(10, 275, 120, 30))
         self.spindleSizeLabel.setObjectName("spindleSizeLabel")
-        
+
         self.tamanioHusillo = QtWidgets.QSpinBox(self.frame_2)
-        self.tamanioHusillo.setGeometry(QtCore.QRect(10, 310, 120, 30)) 
+        self.tamanioHusillo.setGeometry(QtCore.QRect(10, 310, 120, 30))
         self.tamanioHusillo.setMaximum(99999)
         self.tamanioHusillo.setObjectName("tamanioHusillo")
         self.tamanioHusillo.setValue(data[6])
-        
-        #Platform        
+
+        # Platform
         self.platformLabel = QtWidgets.QLabel(self.frame_2)
         self.platformLabel.setGeometry(QtCore.QRect(10, 345, 120, 30))
         self.platformLabel.setObjectName("platformLabel")
-                
+
         self.platform = QtWidgets.QSpinBox(self.frame_2)
-        self.platform.setGeometry(QtCore.QRect(10, 380, 120, 30)) 
+        self.platform.setGeometry(QtCore.QRect(10, 380, 120, 30))
         self.platform.setMaximum(99999)
         self.platform.setObjectName("platform")
         self.platform.setValue(data[7])
-        
-        
-        
+
         self.retranslateUi(interfaz)
 
         QtCore.QMetaObject.connectSlotsByName(interfaz)
-        
+
     def retranslateUi(self, interfaz):
         _translate = QtCore.QCoreApplication.translate
         interfaz.setWindowTitle(_translate("CaptuRing", "CaptuRing"))
         self.imageL.show()
-        
 
         self.addressL.setText(_translate("CaptuRing", self.urlLink))
-        
+
         self.shootingL.setText(_translate("CaptuRing", "Image Shooting"))
         self.optionsL.setText(_translate("CaptuRing", "Options"))
-        
-        #self.nameLabel.setText(_translate("CaptuRing", "Name of the Panorama"))
+
         self.nameCoreL.setText(_translate("CaptuRing", "Sample Name"))
-        #self.stitch.setText(_translate("CaptuRing", "STITCH"))
-        #self.panoramasList.setItemText(0, _translate("CaptuRing", "None"))
-        #self.newPanorama.setText(_translate("CaptuRing", "NewP"))
+
         self.newcoreName.setText(_translate("CaptuRing", "Name It!"))
-        #self.panoramasLabel.setText(_translate("CaptuRing", "Panoramas"))
-        #self.obtainImg.setText(_translate("CaptuRing", "Obtain"))
-        #self.addImg.setText(_translate("CaptuRing", "Add"))
-        #self.imagenesLabel.setText(_translate("CaptuRing", "Images"))
-        #self.imagenesList.setItemText(0, _translate("CaptuRing", "Images"))
-        #self.removeImg.setText(_translate("CaptuRing", "Remove"))
-        self.sampleSizeLabel.setText(_translate("CaptuRing", "Sample size (mm)"))
+
+        self.sampleSizeLabel.setText(
+            _translate("CaptuRing", "Sample size (mm)"))
         self.offsetLabel.setText(_translate("CaptuRing", "Offset"))
         self.spindleSizeLabel.setText(_translate("CaptuRing", "Spindle size"))
         self.sizeStepLabel.setText(_translate("CaptuRing", "Step size"))
@@ -279,8 +181,3 @@ class Ui_interfaz(object):
         self.platformLabel.setText(_translate("CaptuRing", "Platform"))
         self.spstepLabel.setText(_translate("CaptuRing", "Speed/step"))
         self.inSpeedLabel.setText(_translate("CaptuRing", "Initial speed"))
-
-        
-        
-
-
